04fa9172c3a45a668e55610a80642687f0b9a506
Vitaly Chibrikov <vitaly.chibrikov@gmail.com>
2018-04-13 19:36:01 +0300

    Lottery

--- lottery/src/main/java/ru/otus/lottery/MainStream.java
+++ lottery/src/main/java/ru/otus/lottery/MainStream.java
@@ -15,7 +15,33 @@ https://github.com/vitaly-chibrikov/otus_java_2018_01
 
 public class MainStream {
     public static void main(String[] args) throws IOException {
-        String salt = "";
+        String salt_otus = "gr_buza\u200B\"учение -\uFEFF свет, а неучение - чуть свет и на работу\"\n" +
+                "\n" +
+                "Hunan Abrahamyan\u200BЗа\uFEFF державу обидно\n" +
+                "\n" +
+                "Dmitry Syusin\u200Bна душе всё\uFEFF спокойно)\n" +
+                "\n" +
+                "Александр Александрович\u200B\"Вы\uFEFF - соль земли\" Иисус\n" +
+                "\n" +
+                "Филипп Ануфриев\u200Bбалерина крутится лавеха мутится\uFEFF\n" +
+                "\n" +
+                "Dmitry Lyutenko\u200BShow\uFEFF Must Go ON!!\n" +
+                "\n" +
+                "Никита Абраменко\u200Bя вообще питонист пожелайте мне удачи на доде по\uFEFF питоне)";
+
+        String salt = "Миллер Фридрих\u200Bхм..я должен\uFEFF выйграть\n" +
+                "\n" +
+                "Alinka021296\u200Bмногозначительный комментарий\uFEFF\n" +
+                "\n" +
+                "Hunan Abrahamyan\u200BНе\uFEFF сыпь мне соль на рану\n" +
+                "\n" +
+                "Миллер Фридрих\u200Bend\uFEFF\n" +
+                "\n" +
+                "Evgeny Ageev\u200B\"Слабые с доступными,\uFEFF сильные с достойными\"\n" +
+                "\n" +
+                "Никита Абраменко\u200Bпитон\uFEFF лучше джавы\n" +
+                "\n" +
+                "Миллер Фридрих\u200Bбольше\uFEFF не вставим";
 
         Files.lines(Paths.get(args[0]))
                 .map(String::trim)
